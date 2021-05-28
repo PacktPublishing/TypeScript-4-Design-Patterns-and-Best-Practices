79b5922bd57e4fb0389823d369b5f6cbbe397983
theodesp <theo.despoudis@wpengine.com>
2021-05-28 10:36:05 +0100

    updates

--- chapters/chapter-4_Structural_Design_Patterns/Bridge.ts
+++ chapters/chapter-4_Structural_Design_Patterns/Bridge.ts
@@ -57,10 +57,6 @@ sc.arrangeItem({
   value: "dfa",
 });
 console.log(sc.items);
-[
-  { id: "3", value: "dfa" },
-  { id: "1", value: "abc" },
-];
 
 // Implementor type
 interface StoreAPI<T> {
@@ -68,8 +64,9 @@ interface StoreAPI<T> {
 }
 // Abstraction type
 interface List<T> {
-  push(item: T);
+  push(item: T): void;
 }
+
 class ArrayList<T> implements List<T> {
   constructor(private items: T[], private storeAPI: StoreAPI<T>) {}
   push(item: T): void {
@@ -77,7 +74,16 @@ class ArrayList<T> implements List<T> {
   }
   // implements methods of List
 }
+interface ListNode<T> {
+  data: T;
+  next: ListNode<T> | null;
+}
 class LinkedList<T> implements List<T> {
-  constructor(private root: Node, private items: T[]) {}
+  constructor(private root: ListNode<T>, private items: T[]) {}
   // implements methods of List
+  push(item: T): void {
+    const newNode: ListNode<T> = {data: item, next: null};
+    newNode.next = this.root;
+    this.root = newNode;
+  }
 }
